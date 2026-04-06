5807ca75381ff9bd7c49b810838a602df4b9e90f
LYDONLUCIEN <q510971228@gmail.com>
2026-04-06 14:39:17 +0800

    stop tracking uidesign directory

--- uidesign/reference/dashboard/src/app/Root.tsx
+++
@@ -1,8 +0,0 @@
-import { LandingPage } from './components/LandingPage';
-import { useState } from 'react';
-
-export default function Root() {
-  const [language] = useState<'en' | 'zh'>('zh');
-
-  return <LandingPage language={language} />;
-}
